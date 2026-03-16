821b67fcdf3784681625551d44b77854a3270c65
刘白菜 <liushuai.baicai@hotmail.com>
2026-03-16 20:13:39 +0800

    feat: 更新请求处理方式
    - 将请求处理从使用 `handle` 改为使用 `getRequestListener`。
    - 绑定 `app.fetch` 方法以适应新的请求处理方式。

--- api/index.mjs
+++ api/index.mjs
@@ -1,4 +1,4 @@
-import { handle } from "hono/vercel";
+import { getRequestListener } from "@hono/node-server";
 import app from "../dist/app.js";
 
-export default handle(app);
+export default getRequestListener(app.fetch.bind(app));
